953104ea40d6eda084a193d721b0d418841d8554
Yasmine <yasminemedhat97@gmail.com>
2019-07-30 13:20:25 +0200

    Impelement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       # Handle a successful save-> redirect to @user
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
     else
